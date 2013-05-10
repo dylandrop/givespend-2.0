c0951e9c82f337e2bbb9e3ff1555631944f831d9
Dylan <dylandrop@gmail.com>
2013-05-09 21:06:26 -0400

    added routes for devise and authentications

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Givespend::Application.routes.draw do
-  get "authentications/new"
-
-  get "authentications/index"
-
-  get "authentications/create"
-
-  get "users/new"
-
-  get "users/create"
-
-  get "users/show"
-
-  devise_for :users
-
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'new' }
+  resources :authentications, only: [:index, :new, :create]
+  match 'users/auth/:provider/callback' => 'authentications#create'
   resources :nonprofits, only: [:index, :show]
   resources :items, only: [:index,:new,:create,:show]
   resources :categories, only: [:show]
