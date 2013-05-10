ee322400924de2474f6f15adad897cb9398757d0
Dylan <dylandrop@gmail.com>
2013-05-09 20:55:20 -0400

    model for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130510003322) do
+ActiveRecord::Schema.define(:version => 20130510005415) do
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "key"
+    t.string   "secret"
+    t.string   "code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "authentications", ["user_id"], :name => "index_authentications_on_user_id"
 
   create_table "categories", :force => true do |t|
     t.string   "name"
