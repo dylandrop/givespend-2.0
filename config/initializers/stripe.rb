ce6c7a24cf621e307f6ecd84008a8188115f2de2
Dylan <dylandrop@gmail.com>
2013-05-24 16:13:29 -0700

    Added verification for credit cards. Now, onto creating models for Transactions

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,6 +2,7 @@ stripe_config = YAML::load(File.open("#{Rails.root}/config/stripe.yml"))
 STRIPE_CLIENT_ID = stripe_config[Rails.env]['client_id']
 STRIPE_SECRET = stripe_config[Rails.env]['stripe_secret']
 STRIPE_PUBLIC = stripe_config[Rails.env]['stripe_public']
+Stripe.api_key = STRIPE_SECRET
 
 Devise.setup do |config|
   config.omniauth :stripe_connect, STRIPE_CLIENT_ID, STRIPE_SECRET, :scope => 'read_write', :stripe_landing => 'register'
