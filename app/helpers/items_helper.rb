a4cc91f9abb080849719aad6a3784b91c1fa064f
Dylan <dylandrop@gmail.com>
2013-07-27 21:45:41 -0400

    Attempt to fix acronymize

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,9 +1,9 @@
 module ItemsHelper
 	def color_class
-		["red-card", "teal-card"].sample
+		["red-card", "teal-card", ""].sample
 	end
 
   def acronymize some_title
-    (/^[A-Z]+$/.match(some_title) && some_title.length > 10) ? some_title : some_title.gsub(/(\w)\w+\W*/, '\1').upcase
+    (/^[A-Z]+$/.match(some_title) || some_title.length < 11) ? some_title : some_title.gsub(/(\w)\w+\W*/, '\1').upcase
   end
 end
