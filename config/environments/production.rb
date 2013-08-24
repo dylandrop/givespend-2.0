68cfd976f55bc0bd59a9e73496a66c1255ed2ed1
Dylan <dylandrop@gmail.com>
2013-08-24 14:35:44 -0400

    Password can be reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Givespend::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.default_url_options = { :host => 'givespend.com' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
