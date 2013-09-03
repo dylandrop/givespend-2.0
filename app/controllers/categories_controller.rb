6393a94953b3f5c70d12f45b406ef0baf7deee6d
Dylan <dylandrop@gmail.com>
2013-09-03 13:57:48 -0400

    Meaningful titles on (most) pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find(params[:id])
     @items = Item.active.where(category_id: params[:id]).page(params[:page]).per(8).includes(:nonprofit, :image)
   end
 end
